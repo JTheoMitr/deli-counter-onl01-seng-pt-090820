b46ebd4c7b55490e6c5a2993c3018a6c44b54e17
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-09-17 07:01:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,11 @@
-katz_deli = []
-
 def line(array)
-  the_line = "The line is currently"
+  if array.length >= 1
+    new_array = []
+    array.each.with_index(1) { |name, array_index|
+    new_array << ("#{array_index}. #{name}")
+  }
+    puts "The line is currently: #{new_array.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
